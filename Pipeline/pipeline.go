a9b109ff3eb0b0aa3f0e8f8c1e56fba943580329
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-10-02 01:35:50 +0200

    pipeline: add comments

--- Pipeline/pipeline.go
+++ Pipeline/pipeline.go
@@ -5,22 +5,28 @@ import (
 	"io/ioutil"
 	"os"
 	"regexp"
-	sorted "sort"
+	sorting "sort" // to match the python code we have a variable called sort
 	"strings"
 )
 
-type PairList []Pair
+// equivalent of Python's dictionary items
+// from https://github.com/tvraman/go-learn/blob/master/pairlist/pairlist.go
 type Pair struct {
 	Key   string
 	Value int
 }
 
+// this adds the ability to sort pairs
+type PairList []Pair
 func (p PairList) Len() int           { return len(p) }
 func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
 func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
+// equivalent to Python's string.ascii_lowercase
 var ascii_lowercase = "abcdefghijklmnopqrstuvwxyz"
 
+// equivalent of Python's x in y
+// from https://stackoverflow.com/a/15323988
 func stringInSlice(a string, list []string) bool {
 	for _, b := range list {
 		if b == a {
@@ -30,6 +36,8 @@ func stringInSlice(a string, list []string) bool {
 	return false
 }
 
+//Takes a path to a file and returns the entire
+//contents of the file as a string
 func readFile(pathToFile string) string {
 	f, err := ioutil.ReadFile(pathToFile)
 	if err != nil {
@@ -39,15 +47,22 @@ func readFile(pathToFile string) string {
 	return data
 }
 
+//Takes a string and returns a copy with all nonalphanumeric
+//chars replaced by white space
 func filterCharsAndNormalize(strData string) string {
 	pattern := regexp.MustCompile(`[\W_]+`)
 	return strings.ToLower(pattern.ReplaceAllString(strData, ` `))
 }
 
+//Takes a string and scans for words, returning
+//a list of words.
 func scan(strData string) []string {
 	return strings.Fields(strData)
 }
 
+
+//Takes a list of words and returns a copy with all stop
+//words removed
 func removeStopWords(wordList []string) []string {
 	f, err := ioutil.ReadFile(os.Args[2])
 	if err != nil {
@@ -65,6 +80,8 @@ func removeStopWords(wordList []string) []string {
 	return filteredWordList
 }
 
+//Takes a list of words and returns a dictionary associating
+//words with frequencies of occurrence
 func frequencies(wordList []string) map[string]int {
 	wordFreqs := map[string]int{}
 	for _, word := range wordList {
@@ -77,6 +94,9 @@ func frequencies(wordList []string) map[string]int {
 	return wordFreqs
 }
 
+//Takes a dictionary of words and their frequencies
+//and returns a list of pairs where the entries are
+//sorted by frequency
 func sort(wordFrequ map[string]int) PairList {
 	pairList := make(PairList, len(wordFrequ))
 	i := 0
@@ -84,10 +104,11 @@ func sort(wordFrequ map[string]int) PairList {
 		pairList[i] = Pair{word, frequency}
 		i++
 	}
-	sorted.Sort(sorted.Reverse(pairList))
+	sorting.Sort(sorting.Reverse(pairList))
 	return pairList
 }
 
+//Takes a list of pairs where the entries are sorted by frequency and print them recursively.
 func printAll(wordFreqs PairList, limit int) {
 	if len(wordFreqs) > 0 && limit > 0 {
 		fmt.Println(wordFreqs[0].Key, " - ", wordFreqs[0].Value)
@@ -95,6 +116,7 @@ func printAll(wordFreqs PairList, limit int) {
 	}
 }
 
+// The main function
 func main() {
 	printAll(sort(frequencies(removeStopWords(scan(filterCharsAndNormalize(readFile(os.Args[1])))))), 25)
 }
